16527d3dc4f5064aa6ca28603ec0db046a9c3404
gabriel <nobrecoder@gmail.com>
2024-06-10 12:50:37 -0300

    commit in 10/06/2024 12:50:29

--- src/br/com/nobrecoder/operations/animal/Cat.java
+++ src/br/com/nobrecoder/operations/animal/Cat.java
@@ -1,5 +1,8 @@
 package br.com.nobrecoder.operations.animal;
 
+/* Note que o atributo "meow" é privado, portanto, não deveria ser possível acessá-lo sem o uso de reflexão!
+ * */
+
 public class Cat {
 	
 	private String meow = "Miauu!!";
